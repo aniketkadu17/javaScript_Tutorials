371510a673106352058ff219dbff03e189e4dd6c
Aniket Kadu <aniket.kadu17@gmail.com>
2021-11-22 14:58:15 +0530

    Add operators file
    with understanding different types of operators in javascript

---
+++ 01Basics/operators.js
@@ -0,0 +1,19 @@
+// + - * / %
+// < > = <= >=
+// ! & && | ||
+
+var num1 = 5;
+var num2 = 6;
+console.log(num1 * num2);
+
+var ans = num1 + num2;
+console.log("Addition :"+ans);
+
+// keep maintain your BODMAS formated equation code
+((a + b) * c) / (d - e)
+
+var sellingPrice = 199;
+var costPrice = 499;
+
+var discountPrice = Math.round(((costPrice - sellingPrice)/costPrice)*100)
+console.log("Discount price is :"+discountPrice+ " % off");
