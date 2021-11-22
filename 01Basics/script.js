b382a0e48190813e6fab7b61a4dccc0cbd8c81d7
Aniket Kadu <aniket.kadu17@gmail.com>
2021-11-22 14:01:26 +0530

    Add script file
    Understanding variables and data types in JS

---
+++ 01Basics/script.js
@@ -0,0 +1,46 @@
+// var datatype
+
+var name = "Aniket";
+var email = "aniketkadu@gmail.com"
+var designation = "Python Developer";
+var  isSalaried = true;
+var age = 23;
+var isLoggedInFromGoogle = true;
+var hobbies;
+hobbies = "Play Chess"
+
+
+// console.log("Python Developer");
+// console.log(hobbies);
+justTring = "Un-named Variable"
+
+// const datatype
+
+const uid = "abc123";
+// This is Immutable data type
+
+
+var designation = "Python Developer";
+var passward = "Aniket@123456789";
+
+// Popup box OR output box OR alert box
+// works only in browser
+//      alert("Enter your name");
+
+// Popup Input box OR user i/p box
+// works only in browser
+//      prompt("Enter your name");
+
+// out put can be seen in console section
+// sounds like simple print function
+console.log("Emplyee Full name is: "+name);
+console.log("Emplyee mail id is: "+email);
+
+
+// ` back coat and ' forward/ normal coat
+console.log(`
+    With Unique ID: ${uid}
+    User id : ${name}
+    and his mail is: ${email}
+    with password: ${passward}
+`);
