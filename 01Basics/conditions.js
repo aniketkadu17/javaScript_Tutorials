327a32de31898d6e3333efd01285aae55c1bfccf
Aniket Kadu <aniket.kadu17@gmail.com>
2021-11-23 12:11:30 +0530

    Learning conditions in JS

---
+++ 01Basics/conditions.js
@@ -0,0 +1,14 @@
+var number;
+
+// TODO : go to google ang getting your data 
+
+number = 25
+
+if (number%2==0){
+    console.log("Hey it's a Even number");
+}
+else{
+    console.log("Well  i found this as Odd number");
+}
+
+// nested and ladder if else condition
