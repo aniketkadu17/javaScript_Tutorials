2880463af4bef50e7ca8991cdb9623fa8970a62e
Aniket Kadu <aniket.kadu17@gmail.com>
2021-11-24 15:59:19 +0530

    Conditional IF/ ELSE statements
    basic understanding and using Nested IF/ Else

--- 01Basics/conditions.js
+++ 01Basics/conditions.js
@@ -1,14 +1,34 @@
 var number;
-
-// TODO : go to google ang getting your data 
-
 number = 25
 
+// Whenever you find Curly bracese { ITS a CODE BLOCK }
 if (number%2==0){
     console.log("Hey it's a Even number");
-}
-else{
+} else if (number%2!=0){
     console.log("Well  i found this as Odd number");
+} else {
+    console.log("This is Invalid Entry");
 }
 
 // nested and ladder if else condition
+// Google varification
+var userLogin = "username"
+var Password = "password"
+var userType = "Admin"
+if (userLogin == "username") {
+
+    if (Password == "password") {
+        
+        if (userType == "user") {
+            console.log("This is User Page")
+        } else if (userType == "admin") {
+            console.log("This is Admin Page")
+        } else{
+            console.log("This is Visitor Page with limited access")
+        }
+    } else{
+        console.log("Password is Invalid")
+    }
+} else{
+    console.log("No Such user found")
+}
