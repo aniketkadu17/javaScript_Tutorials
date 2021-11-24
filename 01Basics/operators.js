3a913968a50d4524380988b7f063d84daa5fae25
Aniket Kadu <aniket.kadu17@gmail.com>
2021-11-24 15:11:58 +0530

    Understanding Null, Undefined and Boolean values

--- 01Basics/operators.js
+++ 01Basics/operators.js
@@ -1,6 +1,7 @@
 // + - * / %
 // < > = <= >=
 // ! & && | ||
+//  ===
 
 var num1 = 5;
 var num2 = 6;
@@ -10,10 +11,40 @@ var ans = num1 + num2;
 console.log("Addition :"+ans);
 
 // keep maintain your BODMAS formated equation code
-((a + b) * c) / (d - e)
+
+// ((a + b) * c) / (d - e)
 
 var sellingPrice = 199;
 var costPrice = 499;
 
 var discountPrice = Math.round(((costPrice - sellingPrice)/costPrice)*100)
 console.log("Discount price is :"+discountPrice+ " % off");
+
+let tempInFahrenheit = 96
+let celsius = (tempInFahrenheit -32) * 5/9
+console.log(celsius)
+
+
+// Null and Undefined Values
+var temp = 0
+temp =2
+console.log("Current temperature is : "+ temp)
+
+var undefined
+console.log("Current temperature is : "+ undefined)
+
+// BOOLEAN values
+
+/*Grade system
+if 
+Grade is A - Excellent
+Grade is B - Very Good
+Grade is C -    Good
+Grade is D - SAtisfactory
+*/
+
+let grade = true
+
+console.log(grade)
+
+// and using conditions we can check TRUE and FALSE is. 0 and 1 as boolean values
