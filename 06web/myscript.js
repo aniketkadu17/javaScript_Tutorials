2f5f41910843c7cd934b6f48ba119bdf2f5fe8ba
Aniket Kadu <aniket.kadu17@gmail.com>
2021-12-01 11:50:12 +0530

    Basics of DOM in js

---
+++ 06web/myscript.js
@@ -0,0 +1,11 @@
+// DOM Document Object Model
+
+console.log(document);
+
+console.log(document.title);
+
+console.log(document.head);
+
+console.log(document.body);
+
+console.log(document.documentURI);
